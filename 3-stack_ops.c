60c79d0e708343aca1926b1901fa087e3c6c16b6
yonroa <yonanderson11@gmail.com>
2022-05-19 11:55:13 -0500

    Fix Betty

--- 3-stack_ops.c
+++ 3-stack_ops.c
@@ -4,20 +4,20 @@
  * mod_stack - computes the rest of the division of the
  * second top element of the stack by the top
  * element of the stack
- * @stack: the stack to be analize 
+ * @stack: the stack to be analize
  * @number_line: the number line
- * Return: the new stack 
+ * Return: the new stack
  */
 void mod_stack(stack_t **stack, unsigned int number_line)
 {
-    stack_t *tmp = *stack;
+	stack_t *tmp = *stack;
 
 	if (!tmp || !tmp->next)
 	{
 		fprintf(stderr, "L%u: can't mod, stack too short\n", number_line);
 		exit(EXIT_FAILURE);
 	}
-    if (tmp->n == 0)
+	if (tmp->n == 0)
 	{
 		fprintf(stderr, "L%u: division by zero\n", number_line);
 		exit(EXIT_FAILURE);
